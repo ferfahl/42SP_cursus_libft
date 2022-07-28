a46bd43d07617811bc8ce44b6bd411d8830b2fc2
ferfahl <fe.alves.fahl@gmail.com>
2022-07-27 23:37:49 -0300

    gnl 27-07

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,36 +12,67 @@
 
 #include "get_next_line.h"
 
+
+size_t	ft_strlen_mod(const char *str, char a, size_t count)
+{
+	while (str[count] != a)
+	{
+		count++;
+	}
+	return (count);
+}
+
 void   get_next_line(int fd) //char *
 {
     char *dest;
     char *temp;
-    int index;
+    static char *rest;
+    size_t index;
     int size;
+    int second_index;
+    int another;
 
     //verificações de erro
     //fd < 0/
     index = 0;
-    temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    
-    read(fd, temp, BUFFER_SIZE);
-//    printf("%s\n", temp);
-    temp[BUFFER_SIZE] = '\0';
-    while (temp[index] != '\n')
+    second_index = 0;
+    size = 0;
+    another = 0;
+/*     if (rest)
     {
-        size++;
-        index++;
-    }
-    index = 0;
+        
+    } */
+    temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    read(fd, temp, BUFFER_SIZE);
+    printf("%s\n", temp);
+    temp[BUFFER_SIZE] = '\0';
+    size = ft_strlen_mod(temp, '\n', index);
     dest = malloc((size + 2) *  sizeof(char));
     while (temp[index] != '\n')
     {
         dest[index] = temp[index];
         index++;
     }
-    dest[index] = '\n';
-    index++;
-    dest[index] = '\0';
-    //adicionar um enter a mais na saída do dest
+    printf("1 index %ld\n size %d\n second_index %d\n", index, size, second_index);
+    if (temp[index] != '\0')
+    {
+        size = ft_strlen_mod(temp, '\n', index);
+        printf("2 index %ld\n size %d\n second_index %d\n", index, size, second_index);
+        rest = malloc((size + 1) * sizeof(char));
+        printf("3 index %ld\n size %d\n second_index %d\n", index, size, second_index);
+        another = index;
+        printf("4 index %ld\n size %d\n second_index %d\n", index, size, second_index);
+        while (temp[index] != '\0')
+        {
+            rest[second_index] = temp[index];
+            index++;
+            second_index++;
+        }
+        printf("5 index %ld\n size %d\n second_index %d\n", index, size, second_index);
+    }
+    printf("%s\n\n", rest);
+    dest[another] = '\n'; //adicionar um enter a mais na saída do dest
+    another++;
+    dest[another] = '\0';
     printf("%s", dest);
 }
